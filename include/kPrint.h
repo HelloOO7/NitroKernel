fc242334bb82228c9e1a496f6b6bf3559151e43d
HelloOO7 <cendarehor@gmail.com>
2022-11-12 19:34:04 +0100

    User-customizable print routine.

--- include/kPrint.h
+++ include/kPrint.h
@@ -5,6 +5,14 @@
 #include "Util/exl_Print.h"
 
 namespace k {
+    class Printer {
+    public:
+        virtual void Print(const char* str) = 0;
+        virtual void Printf(const char* format, va_list va) = 0;
+    };
+
+    extern "C" K_PUBLIC void kPrintSetSystemPrinter(Printer* printer);
+
     K_PUBLIC void Print(const char* str);
 
     K_PUBLIC void Printf(const char* format, ...);
--- src/kPrint.cpp
+++ src/kPrint.cpp
@@ -6,16 +6,29 @@
 #include "Util/exl_Print.h"
 
 namespace k {
+
+    static Printer* g_kSystemPrinter = nullptr;
+
+    extern "C" void kPrintSetSystemPrinter(Printer* printer) {
+        g_kSystemPrinter = printer;
+    }
+
     void Print(const char* str) {
         exlPrint(str);
+        if (g_kSystemPrinter) {
+            g_kSystemPrinter->Print(str);
+        }
     }
 
     void Printf(const char* format, ...) {
         va_list args;
         va_start(args, format);
-        char outBuffer[1024];
-        vsnprintf(outBuffer, 1024, format, args);
+        char outBuffer[256];
+        vsnprintf(outBuffer, 256, format, args);
         exlPrint(outBuffer);
+        if (g_kSystemPrinter) {
+            g_kSystemPrinter->Printf(format, args);
+        }
         va_end(args);
     }
 }
